1a4dd58327d083586d1a518a9677cab9f8ba6417
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 12:34:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+// updateDOM();
+// });
+//
+// function updateDOM(){
+// let element =document.getElementById( "text" );
+// element.innerText= "This is really cool!";
+// }
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+  }
