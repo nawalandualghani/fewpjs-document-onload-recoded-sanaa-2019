f3f5ebbe4c7288b0834dc06661b316868a159c92
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 12:33:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // document.addEventListener("DOMContentLoaded", function() {
 // updateDOM();
 // });
-// 
+//
 // function updateDOM(){
 // let element =document.getElementById( "text" );
 // element.innerText= "This is really cool!";
